2488f473fd6a808d56e8f264b9f54f9362276aca
Hiroyuki Ueta <empf528@gmail.com>
2017-09-24 09:34:34 +0000

    bootstrap design2

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,9 +6,9 @@
 <h1>taskを作りますよー</h1>
 
 <div class="row">
-    <div class="col-sm-offset-2 col-lg-offset-3">
-        {!! Form::model($task, ['route' => 'tasks.store']) !!}
-        
+    
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
         <div class="form-group">
             {!! Form::label('status', 'status:') !!}
             {!! Form::text('status', null, ['class' => 'form-control']) !!}
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -7,7 +7,8 @@
 <h1>id: {{ $task->id }}のtaskを編集する</h1>
 
 <div class="row">
-    <div class="col-sm-offset-2 col-lg-offset-3">
+    
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
         <div class="form-group">
             {!! Form::label('status', 'status:') !!}
